5108bf38e0858468fb3538f68ed2c915b98e80ce
AckoLF <acko@lotusflare.com>
2019-04-15 22:33:38 +0200

    Add basicParallelImplementation

--- MPS_DZ1_OpenMP/sgemm/sgemm.cc
+++ MPS_DZ1_OpenMP/sgemm/sgemm.cc
@@ -80,18 +80,52 @@ void basicSgemm( char transa, char transb, int m, int n, int k, float alpha, con
   }
 }
 
-int main (int argc, char *argv[]) {
+void basicParallelImplementation(char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc )
+{
+    if ((transa != 'N') && (transa != 'n')) {
+        std::cerr << "unsupported value of 'transa' in regtileSgemm()" << std::endl;
+        return;
+    }
 
+    if ((transb != 'T') && (transb != 't')) {
+        std::cerr << "unsupported value of 'transb' in regtileSgemm()" << std::endl;
+        return;
+    }
+
+    int mm, nn, i;
+    #pragma omp parallel for private(mm, nn, i) shared(A, B, C)
+    for (mm = 0; mm < m; ++mm) {
+        for (nn = 0; nn < n; ++nn) {
+            float c = 0.0f;
+            for (i = 0; i < k; ++i) {
+                float a = A[mm + i * lda];
+                float b = B[nn + i * ldb];
+                c += a * b;
+            }
+            C[mm+nn*ldc] = C[mm+nn*ldc] * beta + alpha * c;
+        }
+    }
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+int main (int argc, char *argv[]) {
   int matArow, matAcol;
   int matBrow, matBcol;
   std::vector<float> matA, matBT;
 
-   if (argc != 4)  
+   if (argc != 4)
   {
       fprintf(stderr, "Expecting three input filenames\n");
       exit(-1);
   }
- 
+
   /* Read in data */
   // load A
   readColMajorMatrixFile(argv[1], matArow, matAcol, matA);
@@ -105,9 +139,18 @@ int main (int argc, char *argv[]) {
   double wallClock = omp_get_wtime();
   // Use standard sgemm interface
   basicSgemm('N', 'T', matArow, matBcol, matAcol, 1.0f, &matA.front(), matArow, &matBT.front(), matBcol, 0.0f, &matC.front(), matArow);
-  printf("time_elapsed: %lf\n", omp_get_wtime() - wallClock);
+  printf("serial_implementation: time_elapsed: %lf seconds\n", omp_get_wtime() - wallClock);
+  writeColMajorMatrixFile(argv[3], matArow, matBcol, matC);
 
-  writeColMajorMatrixFile(argv[3], matArow, matBcol, matC); 
+  std::string output_file = std::string(argv[3]);
+  std::string basic_parallel_output_file = output_file;
+  replace(basic_parallel_output_file, ".txt", "_basic_parallel.txt");
+  std::vector<float> basicParallelC(matArow * matBcol);
+
+  wallClock = omp_get_wtime();
+  basicParallelImplementation('N', 'T', matArow, matBcol, matAcol, 1.0f, &matA.front(), matArow, &matBT.front(), matBcol, 0.0f, &basicParallelC.front(), matArow);
+  printf("basic_parallel_implementation: time_elapsed: %lf seconds\n", omp_get_wtime() - wallClock);
+  writeColMajorMatrixFile(basic_parallel_output_file.c_str(), matArow, matBcol, basicParallelC);
 
   return 0;
 }
